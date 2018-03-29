2d39b33458ecca0904f8b2af1a2983fe4817a50d
Matilda Dahlberg <matildadahlberg@matildas-macbook-pro-3.local>
2018-03-29 11:07:43 +0200

    testAddWO

--- TRACKENDO/Controller/AddWorkoutViewController.swift
+++ TRACKENDO/Controller/AddWorkoutViewController.swift
@@ -5,6 +5,7 @@ class AddWorkoutViewController: UIViewController {
     let segueID = "goGoTableView"
     var workoutList : [Workout]?
     
+    
     @IBOutlet weak var titleTextField: UITextField!
     
     @IBOutlet weak var exerciseTextField: UITextField!
@@ -16,19 +17,26 @@ class AddWorkoutViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        hidden()
         
         
     }
     
     
-
-
+    @IBAction func addWoPressed(_ sender: Any) {
+        exTextField1.isHidden = false
+        setTextField1.isHidden = false
+        repsTextField1.isHidden = false
+        
+    }
+    
+    
     @IBAction func savePressed(_ sender: UIButton) {
         
         let workOut = Workout(title: titleTextField.text!, exercise: exerciseTextField.text! , reps: Int(repsTextField.text!)!, set: Int(setTextField.text!)!)
         
-        createAlertAdd(title: "Saved", message: "Your workout have been saved")
-      
+        createAlertAdd(title: "Saved", message: "Your workout has been saved")
+        
         workoutList?.append(workOut)
         
     }
@@ -52,5 +60,58 @@ class AddWorkoutViewController: UIViewController {
         
     }
     
+    func hidden(){
+        exTextField1.isHidden = true
+        exTextField2.isHidden = true
+        exTextField3.isHidden = true
+        exTextField4.isHidden = true
+        exTextField5.isHidden = true
+        exTextField6.isHidden = true
+        exTextField7.isHidden = true
+        exTextField8.isHidden = true
+        
+        setTextField1.isHidden = true
+        setTextField2.isHidden = true
+        setTextField3.isHidden = true
+        setTextField4.isHidden = true
+        setTextField5.isHidden = true
+        setTextField6.isHidden = true
+        setTextField7.isHidden = true
+        setTextField8.isHidden = true
+        
+        repsTextField1.isHidden = true
+        repsTextField2.isHidden = true
+        repsTextField3.isHidden = true
+        repsTextField4.isHidden = true
+        repsTextField5.isHidden = true
+        repsTextField6.isHidden = true
+        repsTextField7.isHidden = true
+        repsTextField8.isHidden = true
+    }
     
+    @IBOutlet weak var repsTextField8: UITextField!
+    @IBOutlet weak var repsTextField7: UITextField!
+    @IBOutlet weak var repsTextField6: UITextField!
+    @IBOutlet weak var repsTextField5: UITextField!
+    @IBOutlet weak var repsTextField4: UITextField!
+    @IBOutlet weak var repsTextField3: UITextField!
+    @IBOutlet weak var repsTextField2: UITextField!
+    @IBOutlet weak var repsTextField1: UITextField!
+    @IBOutlet weak var setTextField8: UITextField!
+    @IBOutlet weak var setTextField7: UITextField!
+    @IBOutlet weak var setTextField6: UITextField!
+    @IBOutlet weak var setTextField5: UITextField!
+    @IBOutlet weak var setTextField4: UITextField!
+    @IBOutlet weak var setTextField3: UITextField!
+    @IBOutlet weak var setTextField2: UITextField!
+    @IBOutlet weak var setTextField1: UITextField!
+    @IBOutlet weak var exTextField8: UITextField!
+    @IBOutlet weak var exTextField7: UITextField!
+    @IBOutlet weak var exTextField6: UITextField!
+    @IBOutlet weak var exTextField5: UITextField!
+    @IBOutlet weak var exTextField4: UITextField!
+    @IBOutlet weak var exTextField3: UITextField!
+    @IBOutlet weak var exTextField2: UITextField!
+    @IBOutlet weak var exTextField1: UITextField!
 }
+
